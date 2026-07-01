32b5ff1986d99d063d59aac8d97345bd807beb49
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2026-07-01 15:37:46 +0545

    Testing on main

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+import os
+from backend.app.index.indexer import Indexer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATA = os.environ.get("DATA", "data")
+
+if __name__ == "__main__":
+    indexer = Indexer()
+
+    indexer.index_directory(str(DATA))
+
+    print(f"Indexed {len(indexer.documents)} documents.")
+    print(f"Vocabulary size: {indexer.index.vocabulary_size()}.")
