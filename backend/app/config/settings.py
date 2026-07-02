dfaffce548cc32df1a1a008ef0fdf098dd2201c5
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2026-07-03 00:55:19 +0545

    Adding settings configurator

---
+++ backend/app/config/settings.py
@@ -0,0 +1,15 @@
+# backend/app/config/settings.py
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    db_path: str = "./backend/app/data/uriel.db"
+    data_dir: str = "data/documents"
+    default_algorithm: str = "bm25"
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
